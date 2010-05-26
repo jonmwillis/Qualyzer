6fd1ebb214073a0bae044eeb72ee173199d64cc0
Jonathan Faubert <jonfaub@gmail.com>
2010-05-26 15:16:09 -0400

    Add participant Dialog now adds the participant to the project and opens the editor. Right clicking on a participant gives the option to edit a participant which opens the editor.
    Editor is laid-out but does not allow for saving yet.
    
    refs #8.

--- ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/handlers/AddParticipantHandler.java
+++ ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/handlers/AddParticipantHandler.java
@@ -13,11 +13,29 @@ package ca.mcgill.cs.swevo.qualyzer.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.window.Window;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.navigator.CommonNavigator;
 
+import ca.mcgill.cs.swevo.qualyzer.QualyzerActivator;
 import ca.mcgill.cs.swevo.qualyzer.dialogs.AddParticipantDialog;
+import ca.mcgill.cs.swevo.qualyzer.editors.ParticipantFormEditor;
+import ca.mcgill.cs.swevo.qualyzer.editors.inputs.ParticipantEditorInput;
+import ca.mcgill.cs.swevo.qualyzer.model.Code;
+import ca.mcgill.cs.swevo.qualyzer.model.HibernateDBManager;
+import ca.mcgill.cs.swevo.qualyzer.model.Investigator;
+import ca.mcgill.cs.swevo.qualyzer.model.Memo;
 import ca.mcgill.cs.swevo.qualyzer.model.Participant;
+import ca.mcgill.cs.swevo.qualyzer.model.PersistenceManager;
+import ca.mcgill.cs.swevo.qualyzer.model.Project;
+import ca.mcgill.cs.swevo.qualyzer.model.Transcript;
+import ca.mcgill.cs.swevo.qualyzer.providers.ProjectWrapper;
+import ca.mcgill.cs.swevo.qualyzer.util.HibernateUtil;
 
 /**
  * Launches a dialog whenever the New Participant Command is clicked.
@@ -30,6 +48,10 @@ public class AddParticipantHandler extends AbstractHandler
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException
 	{
+		IWorkbenchPage page = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage();
+		CommonNavigator view = (CommonNavigator) page.findView(QualyzerActivator.PROJECT_EXPLORER_VIEW_ID);
+		ISelection selection = page.getSelection();
+		
 		AddParticipantDialog dialog = new AddParticipantDialog(HandlerUtil.getActiveWorkbenchWindow(event).getShell());
 		dialog.create();
 		if(dialog.open() == Window.OK)
@@ -38,10 +60,83 @@ public class AddParticipantHandler extends AbstractHandler
 			participant.setParticipantId(dialog.getParticipantId());
 			participant.setFullName(dialog.getFullname());
 			
-			//TODO add the participant to the relevant project
-			//TODO open the participant editor
+			if (selection != null && selection instanceof IStructuredSelection)
+			{
+				IStructuredSelection strucSelection = (IStructuredSelection) selection;
+				Object element = strucSelection.getFirstElement();
+
+				Project project = getProject(element);
+				
+				project.getParticipants().add(participant);
+				HibernateDBManager manager;
+				manager = QualyzerActivator.getDefault().getHibernateDBManagers().get(project.getName());
+				HibernateUtil.quietSave(manager, project);
+				view.getCommonViewer().refresh();
+				openEditor(participant, page);
+			}
 		}
 		return null;
+	}
+
+	/**
+	 * @param participant
+	 * @param page
+	 */
+	private void openEditor(Participant participant, IWorkbenchPage page)
+	{
+		ParticipantEditorInput input = new ParticipantEditorInput(participant);
+		try
+		{
+			page.openEditor(input, ParticipantFormEditor.ID);
+		}
+		catch (PartInitException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param element The object selected by the user.
+	 * @return TODO
+	 */
+	private Project getProject(Object element)
+	{
+		Project project = null;
+		
+		if(element instanceof IProject)
+		{
+			String projectName = ((IProject) element).getName();
+			project = PersistenceManager.getInstance().getProject(projectName);
+		}
+		else if(element instanceof ProjectWrapper)
+		{
+			project = ((ProjectWrapper) element).getProject();
+		}
+		else if(element instanceof Code)
+		{
+			project = ((Code) element).getProject();
+		}
+		else if(element instanceof Participant)
+		{
+			project = ((Participant) element).getProject();
+		}
+		else if(element instanceof Investigator)
+		{
+			project = ((Investigator) element).getProject();
+		}
+		else if(element instanceof Transcript)
+		{
+			project = ((Transcript) element).getProject();
+		}
+		else if(element instanceof Memo)
+		{
+			project = ((Memo) element).getProject();
+		}
+		else
+		{
+			System.out.println("Error, selected:" +element.getClass().getName());
+		}
+		return project;
 	}
 
 }
