ab098c5034ffd8c23c13e11c556475a56aa42511
Jonathan Faubert <jonfaub@gmail.com>
2010-06-01 15:03:35 -0400

    Import Audio File action - use case 2.2 (refs #9)

--- ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/handlers/ImportAudioFileHandler.java
+++ ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/handlers/ImportAudioFileHandler.java
@@ -10,9 +10,27 @@
  *******************************************************************************/
 package ca.mcgill.cs.swevo.qualyzer.handlers;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import ca.mcgill.cs.swevo.qualyzer.QualyzerActivator;
+import ca.mcgill.cs.swevo.qualyzer.model.AudioFile;
+import ca.mcgill.cs.swevo.qualyzer.model.HibernateDBManager;
+import ca.mcgill.cs.swevo.qualyzer.model.Transcript;
+import ca.mcgill.cs.swevo.qualyzer.util.HibernateUtil;
 
 /**
  * 
@@ -22,11 +40,86 @@ import org.eclipse.core.commands.ExecutionException;
 public class ImportAudioFileHandler extends AbstractHandler
 {
 
+	/**
+	 * 
+	 */
+	private static final String AUDIO = File.separator+"audio"+File.separator;
+
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException
 	{
-		// TODO Auto-generated method stub
+		ISelection selection = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getSelection();
+		if(selection != null)
+		{
+			IStructuredSelection strucSelection = (IStructuredSelection) selection;
+			Object element = strucSelection.getFirstElement();
+			if(element instanceof Transcript)
+			{
+				String projectName = ((Transcript) element).getProject().getName();
+				IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+				String path =  project.getLocation() + AUDIO;
+				
+				String fileName = openFileDialog(event);
+				if(fileName != null)
+				{
+					String ext = fileName.substring(fileName.lastIndexOf('.'));
+					copyFile(element, path, fileName, ext);
+
+					AudioFile audioFile = new AudioFile();
+					audioFile.setRelativePath(AUDIO + ((Transcript) element).getName() + ext);
+					((Transcript) element).setAudioFile(audioFile);
+					
+					HibernateDBManager manager;
+					manager = QualyzerActivator.getDefault().getHibernateDBManagers().get(projectName);
+					HibernateUtil.quietSave(manager, (Transcript) element);
+				}
+			}
+		}
 		return null;
+	}
+
+	/**
+	 * @param event
+	 * @return
+	 */
+	private String openFileDialog(ExecutionEvent event)
+	{
+		FileDialog dialog = new FileDialog(HandlerUtil.getActiveShell(event));
+		dialog.setFilterExtensions(new String[]{"*.mp3;*.wav"});
+		dialog.setFilterNames(new String[]{"Audio (.mp3, .wav)"});
+		
+		String fileName = dialog.open();
+		return fileName;
+	}
+
+	/**
+	 * @param element
+	 * @param path
+	 * @param fileName
+	 * @param ext
+	 */
+	private void copyFile(Object element, String path, String fileName, String ext)
+	{
+		File file = new File(fileName);
+		File fileCpy = new File(path+((Transcript) element).getName()+ext);
+		if(!fileCpy.exists())
+		{
+			try
+			{
+				FileReader in = new FileReader(file);
+				FileWriter out = new FileWriter(fileCpy);
+				
+				int c;
+				while((c = in.read()) != -1)
+				{
+					out.write(c);
+				}
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
