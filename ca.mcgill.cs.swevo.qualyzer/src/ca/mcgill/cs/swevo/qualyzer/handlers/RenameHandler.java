d4707264a54f70c57647ae5d886e8495c9acd697
Jonathan Faubert <jonfaub@gmail.com>
2010-06-02 14:32:16 -0400

    rename command dialog.

--- ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/handlers/RenameHandler.java
+++ ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/handlers/RenameHandler.java
@@ -13,6 +13,17 @@ package ca.mcgill.cs.swevo.qualyzer.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.window.Window;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.navigator.CommonNavigator;
+
+import ca.mcgill.cs.swevo.qualyzer.QualyzerActivator;
+import ca.mcgill.cs.swevo.qualyzer.dialogs.RenameDialog;
+import ca.mcgill.cs.swevo.qualyzer.model.Project;
 
 /**
  * Qualyzer handler for rename (F2).
@@ -26,7 +37,24 @@ public class RenameHandler extends AbstractHandler
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException
 	{
-		System.out.println("rename Handler called");
+		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		ISelection selection = page.getSelection();
+		CommonNavigator view = (CommonNavigator) page.findView(QualyzerActivator.PROJECT_EXPLORER_VIEW_ID);
+		
+		if(selection != null && selection instanceof IStructuredSelection)
+		{
+			Object element = ((IStructuredSelection) selection).getFirstElement();
+			Project project = AddParticipantHandler.getProject(element);
+
+			RenameDialog dialog = new RenameDialog(HandlerUtil.getActiveShell(event).getShell(), project);
+
+			dialog.create();
+			
+			if(dialog.open() == Window.OK)
+			{
+				view.getCommonViewer().refresh();
+			}
+		}
 		return null;
 	}
 
