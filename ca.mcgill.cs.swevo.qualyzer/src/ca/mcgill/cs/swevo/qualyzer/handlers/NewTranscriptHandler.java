686eb630de1193b859b3ec2e9d8077c984848d7f
Jonathan Faubert <jonfaub@gmail.com>
2010-06-11 15:26:21 -0400

    refs #15 added all the logical project elements to the enablement extension of the navigator content: This fixes the refreshing issue.

--- ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/handlers/NewTranscriptHandler.java
+++ ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/handlers/NewTranscriptHandler.java
@@ -13,7 +13,6 @@ package ca.mcgill.cs.swevo.qualyzer.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.WizardDialog;
@@ -53,11 +52,6 @@ public class NewTranscriptHandler extends AbstractHandler
 			{
 				CommonNavigator view = (CommonNavigator) page.findView(QualyzerActivator.PROJECT_EXPLORER_VIEW_ID);
 				view.getCommonViewer().refresh(new WrapperTranscript(wizard.getTranscript().getProject()));
-				
-				if(element instanceof IProject)
-				{
-					view.getCommonViewer().refresh(element);
-				}
 				
 				ResourcesUtil.openEditor(page, wizard.getTranscript());
 			}
