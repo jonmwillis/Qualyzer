b91b16440aabecce679eb8fab66646e3d4b6693c
prmr <prmr@localhost>
2010-06-23 11:17:10 -0400

    refs #23 Redesigned the checking and confirmation logic for Participants. Now only one dialog appears for the entire group.

--- ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/handlers/DeleteParticipantHandler.java
+++ ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/handlers/DeleteParticipantHandler.java
@@ -7,10 +7,12 @@
  *
  * Contributors:
  *     Jonathan Faubert
+ *     Martin Robillard
  *******************************************************************************/
 package ca.mcgill.cs.swevo.qualyzer.handlers;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -35,11 +37,11 @@ import ca.mcgill.cs.swevo.qualyzer.model.Transcript;
 
 /**
  * The handler for the delete participant command.
- *
+ * Multiple participants can be deleted at once, but the operation is atomic.
+ * Either all the selected participants are deleted, or none.
  */
 public class DeleteParticipantHandler extends AbstractHandler
 {
-
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException
 	{
@@ -49,6 +51,8 @@ public class DeleteParticipantHandler extends AbstractHandler
 		
 		if(selection != null && selection instanceof IStructuredSelection)
 		{
+			List<String> conflicts = new ArrayList<String>();
+			List<Participant> toDelete = new ArrayList<Participant>();
 			for(Object element : ((IStructuredSelection) selection).toArray())
 			{				
 				if(element instanceof Participant)
@@ -59,93 +63,120 @@ public class DeleteParticipantHandler extends AbstractHandler
 					HibernateDBManager manager = QualyzerActivator.getDefault().getHibernateDBManagers()
 						.get(project.getName());
 					
-					ArrayList<Object> conflicts = checkForConflicts(participant, project, manager.openSession());
-					
-					if(conflicts.size() > 0)
-					{
-						String errorMsg = printErrors(conflicts);
-						MessageDialog.openError(shell, Messages.getString(
-								"handlers.DeleteParticipantHandler.cannotDelete"), errorMsg); //$NON-NLS-1$
-					}
-					else
-					{
-						boolean check = MessageDialog.openConfirm(shell, Messages.getString(
-								"handlers.DeleteParticipantHandler.deleteParticipant"),  //$NON-NLS-1$
-								Messages.getString("handlers.DeleteParticipantHandler.confirm")); //$NON-NLS-1$
-						
-						if(check)
-						{
-							PersistenceManager.getInstance().deleteParticipant(participant, manager);
-							CommonNavigator view;
-							view = (CommonNavigator) page.findView(QualyzerActivator.PROJECT_EXPLORER_VIEW_ID);
-							view.getCommonViewer().refresh();
-						}
-					}
+					conflicts.addAll(checkForConflicts(participant, project, manager));
+					toDelete.add(participant);
 				}
+			}
+			
+			if(conflicts.size() > 0)
+			{
+				String errorMsg = printErrors(conflicts);
+				MessageDialog.openError(shell, Messages.getString(
+						"handlers.DeleteParticipantHandler.cannotDelete"), errorMsg); //$NON-NLS-1$
+			}
+			else
+			{
+				proceedWithDeletion(page, shell, toDelete);
 			}
 		}
 		return null;
 	}
 
+	private void proceedWithDeletion(IWorkbenchPage page, Shell shell, List<Participant> toDelete) 
+	{
+		String message = "";
+		if(toDelete.size() == 1)
+		{
+			message = Messages.getString("handlers.DeleteParticipantHandler.confirm");
+		}
+		else
+		{
+			message = Messages.getString("handlers.DeleteParticipantHandler.confirmMany");
+		}
+		
+		boolean check = MessageDialog.openConfirm(shell, Messages.getString(
+				"handlers.DeleteParticipantHandler.deleteParticipant"),  //$NON-NLS-1$
+				message); //$NON-NLS-1$
+		
+		if(check)
+		{
+			for(Participant participant : toDelete)
+			{
+				// MR we can just store the managers in a hashtable in the loop above if this is slow
+				HibernateDBManager manager = QualyzerActivator.getDefault().getHibernateDBManagers()
+				.get(participant.getProject().getName()); 
+				PersistenceManager.getInstance().deleteParticipant(participant, manager);
+			}
+			CommonNavigator view;
+			view = (CommonNavigator) page.findView(QualyzerActivator.PROJECT_EXPLORER_VIEW_ID);
+			view.getCommonViewer().refresh();
+		}
+	}
+
 	/**
-	 * @param conflicts
+	 * Formats the list of conflicts into a message string.
 	 */
-	private String printErrors(ArrayList<Object> conflicts)
+	private String printErrors(List<String> conflicts)
 	{
 		String output = Messages.getString("handlers.DeleteParticipantHandler.conflicts"); //$NON-NLS-1$
-		for(Object obj : conflicts)
+		for(Object conflict : conflicts)
 		{
-			if(obj instanceof Memo)
-			{
-				output += Messages.getString(
-						"handlers.DeleteParticipantHandler.memo") + ((Memo) obj).getName(); //$NON-NLS-1$
-			}
-			else if(obj instanceof Transcript)
-			{
-				output += Messages.getString(
-						"handlers.DeleteParticipantHandler.transcript")+ ((Transcript) obj).getName(); //$NON-NLS-1$
-			}
+			output += "\n" + conflict; 
 		}
 		
 		return output;
 	}
 
 	/**
+	 * Determines if the participant is associated with either a memo or a transcript.
+	 * Such an association constituted a conflict.
 	 * @param participant
 	 * @param project
 	 * @param session
-	 * @return
+	 * @return A list of strings describing the conflict.
 	 */
-	private ArrayList<Object> checkForConflicts(Participant participant, Project project, Session session)
+	private ArrayList<String> checkForConflicts(Participant participant, Project project, HibernateDBManager manager)
 	{
-		ArrayList<Object> conflicts = new ArrayList<Object>();
-		for(Memo memo : project.getMemos())
+		Session session = manager.openSession();
+		ArrayList<String> conflicts = new ArrayList<String>();
+		try
 		{
-			Object lMemo = session.get(Memo.class, memo.getPersistenceId());
-			for(Participant part : ((Memo) lMemo).getParticipants())
+			for(Memo memo : project.getMemos())
 			{
-				if(part.equals(participant))
+				Object lMemo = session.get(Memo.class, memo.getPersistenceId());
+				for(Participant part : ((Memo) lMemo).getParticipants())
 				{
-					conflicts.add(memo);
-					break;
+					if(part.equals(participant))
+					{
+						conflicts.add(Messages.getString("handlers.DeleteParticipantHandler.participant") +  
+								participant.getParticipantId() +
+								Messages.getString("handlers.DeleteParticipantHandler.memo") +  
+								memo.getName());
+						break;
+					}
+				}
+			}
+		
+			for(Transcript transcript : project.getTranscripts())
+			{
+				Object lTranscript = session.get(Transcript.class, transcript.getPersistenceId());
+				for(Participant part : ((Transcript) lTranscript).getParticipants())
+				{
+					if(part.equals(participant))
+					{
+						conflicts.add(Messages.getString("handlers.DeleteParticipantHandler.participant") +  
+								participant.getParticipantId() +
+								Messages.getString("handlers.DeleteParticipantHandler.transcript") +  
+								transcript.getName());
+						break;
+					}
 				}
 			}
 		}
-		
-		for(Transcript transcript : project.getTranscripts())
+		finally
 		{
-			Object lTranscript = session.get(Transcript.class, transcript.getPersistenceId());
-			for(Participant part : ((Transcript) lTranscript).getParticipants())
-			{
-				if(part.equals(participant))
-				{
-					conflicts.add(transcript);
-					break;
-				}
-			}
+			session.close();
 		}
-		
-		session.close();
 		return conflicts;
 	}
 
