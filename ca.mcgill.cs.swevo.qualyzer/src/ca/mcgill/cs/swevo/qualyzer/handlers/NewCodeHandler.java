aabec309f806b6fe17731791c4e0f9efd9bddef0
Jonathan Faubert <jonfaub@gmail.com>
2010-06-28 17:01:46 -0400

    refs #29 Added a New->Code item in the context menu. Created a handler for new Code. (Currently does nothing.)

---
+++ ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/handlers/NewCodeHandler.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2010 McGill University
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Jonathan Faubert
+ *******************************************************************************/
+package ca.mcgill.cs.swevo.qualyzer.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+/**
+ * Handler for the new code command.
+ * @author Jonathan Faubert (jonfaub@gmail.com)
+ *
+ */
+public class NewCodeHandler extends AbstractHandler
+{
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
