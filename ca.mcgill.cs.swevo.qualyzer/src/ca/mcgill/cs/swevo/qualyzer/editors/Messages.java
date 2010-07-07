93c70f8c25a53cfef9c941c14e34aa92f6cdf781
Jonathan Faubert <jonfaub@gmail.com>
2010-07-07 12:04:11 -0400

    Cleanup

--- ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/editors/Messages.java
+++ ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/editors/Messages.java
@@ -1,16 +1,24 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2010 McGill University
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Jonathan Faubert
+ *******************************************************************************/
+
+
 package ca.mcgill.cs.swevo.qualyzer.editors;
 
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 /**
- * @author Jonathan Faubert (jonfaub@gmail.com)
  *
  */
-public class Messages
+public final class Messages
 {
 	private static final String BUNDLE_NAME = "ca.mcgill.cs.swevo.qualyzer.editors.messages"; //$NON-NLS-1$
 
@@ -20,6 +28,11 @@ public class Messages
 	{
 	}
 
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
 	public static String getString(String key)
 	{
 		try
