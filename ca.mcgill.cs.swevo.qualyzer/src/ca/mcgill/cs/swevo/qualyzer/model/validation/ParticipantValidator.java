d146da87a22c693ae88d07c2a6acb7e3b8ba3230
Jonathan Faubert <jonfaub@gmail.com>
2010-07-19 11:25:41 -0400

    Cleanup: Externalizing Strings

--- ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/model/validation/ParticipantValidator.java
+++ ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/model/validation/ParticipantValidator.java
@@ -27,7 +27,7 @@ public class ParticipantValidator extends BasicNameValidator
 	 */
 	public ParticipantValidator(String pName, String pOldName, Project pProject)
 	{
-		super(Messages.getString("model.validation.ParticipantValidator.label"), pName, pOldName, pProject);
+		super(Messages.getString("model.validation.ParticipantValidator.label"), pName, pOldName, pProject); //$NON-NLS-1$
 	}
 	
 	/**
