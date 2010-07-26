0509ca550670356682b8bd23d94a77eabcf337d7
prmr <prmr@localhost>
2010-07-26 13:26:36 -0400

    refs #38 Refactored TranscriptValidator

--- ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/model/validation/TranscriptNameValidator.java
+++ ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/model/validation/TranscriptNameValidator.java
@@ -39,7 +39,7 @@ public class TranscriptNameValidator extends BasicNameValidator
 	{
 		for(Transcript transcript : fProject.getTranscripts())
 		{
-			if(!fOldName.equals(fName) && transcript.getName().equals(fName))
+			if(transcript.getName().equals(fName))
 			{
 				return true;
 			}
