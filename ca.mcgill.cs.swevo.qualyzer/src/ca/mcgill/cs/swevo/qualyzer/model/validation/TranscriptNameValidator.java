1f3bdabe230e71bbdab147ddddc187c833d20329
prmr <prmr@localhost>
2010-07-26 13:05:44 -0400

    refs #38 Refactored TranscriptNameValidator

--- ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/model/validation/TranscriptNameValidator.java
+++ ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/model/validation/TranscriptNameValidator.java
@@ -20,11 +20,8 @@ import ca.mcgill.cs.swevo.qualyzer.model.Transcript;
  * - New name not already in use (except if it refers to the current transcript)
  * - New name in alphanumerical+ format.
  */
-public class TranscriptNameValidator extends AbstractValidator
+public class TranscriptNameValidator extends BasicNameValidator
 {
-	private final String fName;
-	private final Project fProject;
-	private String fOldName;
 	/**
 	 * Constructs a new Validator.
 	 * @param pName The name chosen for the new transcript.
@@ -33,37 +30,12 @@ public class TranscriptNameValidator extends AbstractValidator
 	 */
 	public TranscriptNameValidator(String pName, String pOldName, Project pProject)
 	{
-		fName = pName;
-		fOldName = pOldName;
-		fProject = pProject;
+		super(Messages.getString("model.validation.TranscriptNameValidator.label"),
+				pName, pOldName, pProject); 
 	}
 	
 	@Override
-	public boolean isValid() 
-	{
-		boolean lReturn = true;
-		
-		if(fName.length() == 0)
-		{
-			lReturn = false;
-			fMessage = Messages.getString("model.validation.RenameTranscriptValidator.nameEmpty");  //$NON-NLS-1$
-		}
-		else if(!ValidationUtils.verifyID(fName))
-		{
-			lReturn = false;
-			fMessage = Messages.getString(
-					"model.validation.RenameTranscriptValidator.invalidID");  //$NON-NLS-1$
-		}
-		else if(transcriptExists())
-		{
-			lReturn = false;
-			fMessage = Messages.getString("model.validation.RenameTranscriptValidator.nameInUse");  //$NON-NLS-1$
-		}
-		
-		return lReturn;
-	}
-	
-	private boolean transcriptExists()
+	protected boolean nameInUse()
 	{
 		for(Transcript transcript : fProject.getTranscripts())
 		{
