2339b1ecd4c7fa751473cd38a5170d2d6b9c63f9
Jonathan Faubert <jonfaub@gmail.com>
2010-07-30 14:37:14 -0400

    refs #37 Added tests for the new ResourcesUtil actions that have been added due to the addition of memos and codes.

--- ca.mcgill.cs.swevo.qualyzer.tests/src/ca/mcgill/cs/swevo/qualyzer/ui/ResourcesUtilTest.java
+++ ca.mcgill.cs.swevo.qualyzer.tests/src/ca/mcgill/cs/swevo/qualyzer/ui/ResourcesUtilTest.java
@@ -15,13 +15,23 @@ package ca.mcgill.cs.swevo.qualyzer.ui;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import ca.mcgill.cs.swevo.qualyzer.model.Code;
+import ca.mcgill.cs.swevo.qualyzer.model.Facade;
+import ca.mcgill.cs.swevo.qualyzer.model.IAnnotatedDocument;
 import ca.mcgill.cs.swevo.qualyzer.model.Investigator;
 import ca.mcgill.cs.swevo.qualyzer.model.Memo;
 import ca.mcgill.cs.swevo.qualyzer.model.Participant;
@@ -32,6 +42,7 @@ import ca.mcgill.cs.swevo.qualyzer.providers.WrapperInvestigator;
 import ca.mcgill.cs.swevo.qualyzer.providers.WrapperMemo;
 import ca.mcgill.cs.swevo.qualyzer.providers.WrapperParticipant;
 import ca.mcgill.cs.swevo.qualyzer.providers.WrapperTranscript;
+import ca.mcgill.cs.swevo.qualyzer.util.FileUtil;
 
 /**
  * @author Jonathan Faubert (jonfaub@gmail.com)
@@ -39,6 +50,24 @@ import ca.mcgill.cs.swevo.qualyzer.providers.WrapperTranscript;
  */
 public class ResourcesUtilTest
 {
+	private static final String PROJECT_NAME = "ResourcesProject";
+	private static final String INVEST_NAME = "InvestNickName";
+	
+	private Project fProject;
+	
+	@Before
+	public void setUp()
+	{
+		fProject = Facade.getInstance().createProject(PROJECT_NAME, INVEST_NAME, "", "");
+	}
+	
+	@After
+	public void tearDown()
+	{
+		Facade.getInstance().deleteProject(fProject);
+		
+		PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().closeAllEditors(true);
+	}
 
 	/**
 	 * Test the getProject() method.
@@ -46,39 +75,38 @@ public class ResourcesUtilTest
 	@Test
 	public void getProjectTest()
 	{
-		Project project = new Project();
 		
-		Investigator i = new Investigator();
-		i.setProject(project);
+		Investigator i = Facade.getInstance().createInvestigator("bob", "", "", fProject, true);
 		
-		Participant p = new Participant();
-		p.setProject(project);
+		Participant p = Facade.getInstance().createParticipant("pBob", "", fProject);
 		
-		Memo m = new Memo();
-		m.setProject(project);
+		Memo m = Facade.getInstance().createMemo("mmeo", "", i, new ArrayList<Participant>(), fProject, null, null);
 		
-		Transcript t = new Transcript();
-		t.setProject(project);
+		ArrayList<Participant> parts = new ArrayList<Participant>();
+		parts.add(p);
+		Transcript t = Facade.getInstance().createTranscript("trans", "", "", parts, fProject);
 		
-		Code c = new Code();
-		c.setProject(project);
+		Code c = Facade.getInstance().createCode("code", "", fProject);
 		
-		WrapperInvestigator wI = new WrapperInvestigator(project);
-		WrapperParticipant wP = new WrapperParticipant(project);
-		WrapperMemo wM = new WrapperMemo(project);
-		WrapperCode wC = new WrapperCode(project);
-		WrapperTranscript wT = new WrapperTranscript(project);
+		IProject wProject = ResourcesPlugin.getWorkspace().getRoot().getProject(fProject.getName());
 		
-		assertEquals(ResourcesUtil.getProject(i), project);
-		assertEquals(ResourcesUtil.getProject(p), project);
-		assertEquals(ResourcesUtil.getProject(m), project);
-		assertEquals(ResourcesUtil.getProject(t), project);
-		assertEquals(ResourcesUtil.getProject(c), project);
-		assertEquals(ResourcesUtil.getProject(wI), project);
-		assertEquals(ResourcesUtil.getProject(wP), project);
-		assertEquals(ResourcesUtil.getProject(wM), project);
-		assertEquals(ResourcesUtil.getProject(wT), project);
-		assertEquals(ResourcesUtil.getProject(wC), project);
+		WrapperInvestigator wI = new WrapperInvestigator(fProject);
+		WrapperParticipant wP = new WrapperParticipant(fProject);
+		WrapperMemo wM = new WrapperMemo(fProject);
+		WrapperCode wC = new WrapperCode(fProject);
+		WrapperTranscript wT = new WrapperTranscript(fProject);
+		
+		assertEquals(ResourcesUtil.getProject(i), fProject);
+		assertEquals(ResourcesUtil.getProject(p), fProject);
+		assertEquals(ResourcesUtil.getProject(m), fProject);
+		assertEquals(ResourcesUtil.getProject(t), fProject);
+		assertEquals(ResourcesUtil.getProject(c), fProject);
+		assertEquals(ResourcesUtil.getProject(wI), fProject);
+		assertEquals(ResourcesUtil.getProject(wP), fProject);
+		assertEquals(ResourcesUtil.getProject(wM), fProject);
+		assertEquals(ResourcesUtil.getProject(wT), fProject);
+		assertEquals(ResourcesUtil.getProject(wC), fProject);
+		assertEquals(ResourcesUtil.getProject(wProject), fProject);
 
 	}
 	
@@ -86,14 +114,10 @@ public class ResourcesUtilTest
 	 * test open investigator editor.
 	 */
 	@Test
-	public void openEditorTest()
+	public void openInvestigatorEditorTest()
 	{
 		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-		Investigator i = new Investigator();
-		
-		i.setFullName("Jonathan Faubert");
-		i.setNickName("jon");
-		i.setInstitution("Bob Riley University");
+		Investigator i = Facade.getInstance().createInvestigator("Jon", "Jonathan Faubert", "McGill University", fProject, true);
 		
 		ResourcesUtil.openEditor(page, i);
 		
@@ -114,15 +138,10 @@ public class ResourcesUtilTest
 	 * test open participant editor.
 	 */
 	@Test
-	public void openEditorTest2()
+	public void openParticipantEditorTest()
 	{
 		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-		Participant p = new Participant();
-		Project proj = new Project();
-		proj.getParticipants().add(p);
-		
-		p.setFullName("Jonathan Faubert");
-		p.setParticipantId("jon");
+		Participant p = Facade.getInstance().createParticipant("jon", "Jonathan Faubert", fProject);
 		
 		ResourcesUtil.openEditor(page, p);
 		
@@ -133,6 +152,143 @@ public class ResourcesUtilTest
 			{
 				found = true;
 				break;
+			}
+		}
+		
+		assertTrue(found);
+	}
+	
+	@Test
+	public void openCodeEditorTest()
+	{
+		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		
+		WrapperCode codes = new WrapperCode(fProject);
+		
+		ResourcesUtil.openEditor(page, codes);
+		
+		boolean found = false;
+		for(IEditorReference editor : page.getEditorReferences())
+		{
+			if(editor.getName().equals("editor.code."+fProject.getName()))
+			{
+				found = true;
+				break;
+			}
+		}
+		
+		assertTrue(found);
+	}
+	
+	@Test
+	public void openFragmentViewerTest()
+	{
+		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		
+		Code code = Facade.getInstance().createCode("codeID", "desc", fProject);
+		
+		ResourcesUtil.openEditor(page, code);
+		
+		boolean found = false;
+		for(IEditorReference editor : page.getEditorReferences())
+		{
+			if(editor.getName().equals("fragmentViewer."+code.getCodeName()))
+			{
+				found = true;
+				break;
+			}
+		}
+		
+		assertTrue(found);
+	}
+	
+	@Test
+	public void openMemoEditorTest()
+	{
+		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		
+		Memo memo = Facade.getInstance().createMemo("memo", "", fProject.getInvestigators().get(0), null, fProject, null, null);
+		FileUtil.setupMemoFiles(memo.getName(), fProject.getName(), "");
+		
+		ResourcesUtil.openEditor(page, memo);
+		
+		boolean found = false;
+		for(IEditorReference editor : page.getEditorReferences())
+		{
+			try
+			{
+				if(editor.getEditorInput().getName().equals(Memo.class.getSimpleName()+"."+memo.getFileName()))
+				{
+					found = true;
+					break;
+				}
+			}
+			catch (PartInitException e)
+			{
+				e.printStackTrace();
+				fail();
+			}
+		}
+		
+		assertTrue(found);
+	}
+	
+	@Test
+	public void openDocumentEditorTest()
+	{
+		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		
+		Memo memo = Facade.getInstance().createMemo("memo", "", fProject.getInvestigators().get(0), null, fProject, null, null);
+		FileUtil.setupMemoFiles(memo.getName(), fProject.getName(), "");
+		
+		Participant p = Facade.getInstance().createParticipant("participant", "", fProject);
+		ArrayList<Participant> parts = new ArrayList<Participant>();
+		parts.add(p);
+		
+		Transcript transcript = Facade.getInstance().createTranscript("transcript", "", "", parts, fProject);
+		FileUtil.setupTranscriptFiles(transcript.getName(), fProject.getName(), "", "");
+		
+		IAnnotatedDocument document = memo;
+		ResourcesUtil.openEditor(page, document);
+		
+		boolean found = false;
+		for(IEditorReference editor : page.getEditorReferences())
+		{
+			try
+			{
+				if(editor.getEditorInput().getName().equals(Memo.class.getSimpleName()+"."+memo.getFileName()))
+				{
+					found = true;
+					break;
+				}
+			}
+			catch (PartInitException e)
+			{
+				e.printStackTrace();
+				fail();
+			}
+		}
+		
+		assertTrue(found);
+		
+		document = transcript;
+		ResourcesUtil.openEditor(page, document);
+		
+		found = false;
+		for(IEditorReference editor : page.getEditorReferences())
+		{
+			try
+			{
+				if(editor.getEditorInput().getName().equals(Transcript.class.getSimpleName()+"."+transcript.getFileName()))
+				{
+					found = true;
+					break;
+				}
+			}
+			catch (PartInitException e)
+			{
+				e.printStackTrace();
+				fail();
 			}
 		}
 		
