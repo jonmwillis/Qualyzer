037e22ecdfca48045a7e37fb42dd3583414031a6
Jonathan Faubert <jonfaub@gmail.com>
2010-08-03 16:36:56 -0400

    refs #54 Added A menu option for project properties as well as an empty class for the handler and dialog.

---
+++ ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/dialogs/ProjectPropertiesDialog.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2010 McGill University
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Jonathan Faubert
+ *******************************************************************************/
+/**
+ * 
+ */
+package ca.mcgill.cs.swevo.qualyzer.dialogs;
+
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ *
+ */
+public class ProjectPropertiesDialog extends TitleAreaDialog
+{
+	/**
+	 * Constructor.
+	 * @param shell
+	 */
+	public ProjectPropertiesDialog(Shell shell)
+	{
+		super(shell);
+	}
+}
