f70c1062319a0389f3cc920f4d0d7c49f881dc35
Jonathan Faubert <jonfaub@gmail.com>
2010-08-04 14:45:07 -0400

    refs #55 Added the menu item and key binding for rename Proect. Adding the dialog with validation. Added the handler which opens the dialog but does not yet rename the project. And added some functionality to the listener manager which handles the name change for all the map keys.

---
+++ ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/dialogs/RenameProjectDialog.java
@@ -0,0 +1,124 @@
+/*******************************************************************************
+ * Copyright (c) 2010 McGill University
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Jonathan Faubert
+ *******************************************************************************/
+
+package ca.mcgill.cs.swevo.qualyzer.dialogs;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+import ca.mcgill.cs.swevo.qualyzer.model.Project;
+import ca.mcgill.cs.swevo.qualyzer.model.validation.ProjectNameValidator;
+
+/**
+ *
+ */
+public class RenameProjectDialog extends TitleAreaDialog
+{
+	private Project fProject;
+	private String fNewName;
+	
+	private Text fNameText;
+	
+	/**
+	 * @param parentShell
+	 */
+	public RenameProjectDialog(Shell parentShell, Project project)
+	{
+		super(parentShell);
+		fProject = project;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.dialogs.Dialog#create()
+	 */
+	@Override
+	public void create()
+	{
+		super.create();
+		setTitle("Rename Project");
+		setMessage("Please enter a new name for this project.");
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.dialogs.TitleAreaDialog#createDialogArea(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	protected Control createDialogArea(Composite parent)
+	{
+		parent.setLayout(new GridLayout());
+		
+		Composite body = new Composite(parent, SWT.NULL);
+		body.setLayout(new GridLayout(2, false));
+		body.setLayoutData(new GridData(SWT.FILL, SWT.NULL, true, false));
+		
+		Label label = new Label(body, SWT.NULL);
+		label.setText("New Name: ");
+		
+		fNameText = new Text(body, SWT.BORDER);
+		fNameText.setText("");
+		fNameText.setLayoutData(new GridData(SWT.FILL, SWT.NULL, true, false));
+		fNameText.addModifyListener(new ModifyListener()
+		{
+			@Override
+			public void modifyText(ModifyEvent e)
+			{
+				validate();
+			}
+		});
+		
+		return parent;
+	}
+	
+	private void validate()
+	{
+		ProjectNameValidator validator = new ProjectNameValidator(fNameText.getText(), fProject.getName(), fProject);
+		if(validator.isValid())
+		{
+			setErrorMessage(null);
+			getButton(IDialogConstants.OK_ID).setEnabled(true);
+		}
+		else
+		{
+			setErrorMessage(validator.getErrorMessage());
+			getButton(IDialogConstants.OK_ID).setEnabled(false);
+		}
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.dialogs.Dialog#okPressed()
+	 */
+	@Override
+	protected void okPressed()
+	{
+		fNewName = fNameText.getText();
+		super.okPressed();
+	}
+	
+	/**
+	 * @return the NewName
+	 */
+	public String getNewName()
+	{
+		return fNewName;
+	}
+
+}
