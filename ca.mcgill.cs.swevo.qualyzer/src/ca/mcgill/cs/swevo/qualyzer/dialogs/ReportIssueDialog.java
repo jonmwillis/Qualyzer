3e5edd942ff8c58c4d1511723fc933f46d34b2bc
Barthelemy Dagenais <bart@cs.mcgill.ca>
2010-10-01 11:01:29 -0400

    refs #101 Added the generation of log content (System info + Content of Qualyzer log + Content of Workspace Log).

--- ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/dialogs/ReportIssueDialog.java
+++ ca.mcgill.cs.swevo.qualyzer/src/ca/mcgill/cs/swevo/qualyzer/dialogs/ReportIssueDialog.java
@@ -10,6 +10,14 @@
  *******************************************************************************/
 package ca.mcgill.cs.swevo.qualyzer.dialogs;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.eclipse.core.internal.runtime.InternalPlatform;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.internal.adaptor.EclipseEnvironmentInfo;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.Clipboard;
@@ -26,24 +34,26 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.PlatformUI;
 
+import ca.mcgill.cs.swevo.qualyzer.QualyzerActivator;
+import ca.mcgill.cs.swevo.qualyzer.QualyzerException;
+
 /**
  *
  */
+@SuppressWarnings("restriction")
 public class ReportIssueDialog extends TitleAreaDialog
 {
-//	private static Logger gLogger = LoggerFactory.getLogger(ReportIssueDialog.class);
+	// private static Logger gLogger = LoggerFactory.getLogger(ReportIssueDialog.class);
 
-	/**
-	 * 
-	 */
 	private static final int REPORT_HEIGHT = 150;
-
+	private static final int REPORT_WIDTH = 250;
+	
 	private Text fReportText;
-	
+
 	private Button fCopyButton;
-	
+
 	private Clipboard fClipboard;
-	
+
 	/**
 	 * Constructor.
 	 * 
@@ -67,8 +77,10 @@ public class ReportIssueDialog extends TitleAreaDialog
 		setTitle(Messages.getString("dialogs.ReportIssueDialog.title")); //$NON-NLS-1$
 		setMessage(Messages.getString("dialogs.ReportIssueDialog.message")); //$NON-NLS-1$
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.jface.dialogs.TitleAreaDialog#createDialogArea(org.eclipse.swt.widgets.Composite)
 	 */
 	@Override
@@ -82,44 +94,103 @@ public class ReportIssueDialog extends TitleAreaDialog
 		// CSON:
 		GridData gData = new GridData(SWT.FILL, SWT.FILL, true, true);
 		gData.heightHint = REPORT_HEIGHT;
+		gData.widthHint = REPORT_WIDTH;
 		fReportText.setLayoutData(gData);
 		fReportText.setText(getReportText());
-		
+
 		fCopyButton = new Button(composite, SWT.PUSH);
 		fCopyButton.setText(Messages.getString("dialogs.ReportIssueDialog.copyButton"));
 		fCopyButton.addSelectionListener(new SelectionListener()
 		{
-			
+
 			@Override
 			public void widgetSelected(SelectionEvent e)
 			{
 				copyToClipboard();
 			}
-			
+
 			@Override
 			public void widgetDefaultSelected(SelectionEvent e)
 			{
 			}
 		});
-		
+
 		return parent;
 	}
-	
+
+	// CSOFF:
 	private String getReportText()
 	{
-		return "Hello\nWorld!\n\n\n\n\n\n\n\nHello!\n\n\n\n\n\n\n\n\n\n\n\n\nHello!";
+		StringBuilder sBuilder = new StringBuilder();
+
+		sBuilder.append("=== QUALYZER ISSUE REPORT ===\n\n"); //$NON-NLS-1$
+		sBuilder.append("==== System Information ====\n"); //$NON-NLS-1$
+		sBuilder.append(String.format("Qualyzer Version=%s\n", QualyzerActivator.CURRENT_VERSION)); //$NON-NLS-1$
+		sBuilder.append(String.format("Java Version=%s\n", System.getProperty("java.version"))); //$NON-NLS-1$
+		sBuilder.append(String.format("Java Vendor=%s\n", System.getProperty("java.vendor"))); //$NON-NLS-1$
+		sBuilder.append(String.format("BootLoader constants: OS=%s, ARCH=%s, WS=%s, NL=%s\n", 
+								EclipseEnvironmentInfo.getDefault().getOS(), 
+								EclipseEnvironmentInfo.getDefault().getOSArch(), 
+								EclipseEnvironmentInfo.getDefault().getWS(), 
+								EclipseEnvironmentInfo.getDefault().getNL())); //$NON-NLS-1$
+		sBuilder.append(String.format("Command-line arguments: %s\n", 
+								(Object[])EclipseEnvironmentInfo.getDefault().getCommandLineArgs())); //$NON-NLS-1$
+		sBuilder.append("==== End of System Information ====\n\n");
+		
+		sBuilder.append("==== Qualyzer Log ====\n");
+		String qualyzerPath = ResourcesPlugin.getWorkspace().getRoot().getLocation().append("Qualyzer.log").toOSString();
+		sBuilder.append(readFile(qualyzerPath));
+		sBuilder.append("==== End of Qualyzer Log ====\n\n");
+		
+		sBuilder.append("==== Workspace Log ====\n");
+		sBuilder.append(readFile(InternalPlatform.getDefault().getFrameworkLog().getFile().getAbsolutePath()));
+		sBuilder.append("==== End of Workspace Log ====\n\n");
+		
+		sBuilder.append("=== END OF REPORT ===\n"); //$NON-NLS-1$
+
+		return sBuilder.toString();
 	}
+	// CSON:
 	
+	private String readFile(String filePath) 
+	{
+		File file = new File(filePath);
+		if (!file.exists()) 
+		{
+			return String.format("Log file not found: %s\n", filePath);
+		}
+		
+		StringBuilder sBuilder = new StringBuilder();
+		try
+		{
+			BufferedReader reader = new BufferedReader(new FileReader(filePath));
+			String line = null;
+			while ((line = reader.readLine()) != null) 
+			{
+				sBuilder.append(line);
+				sBuilder.append("\n"); //$NON-NLS-1$
+			}
+			reader.close();
+		}
+		catch(IOException ioException)
+		{
+			throw new QualyzerException("Error while reading log file " + filePath, ioException);
+		}
+		
+		return sBuilder.toString();
+	}
+
 	private void copyToClipboard()
 	{
-		
+
 		String textData = fReportText.getText();
-        TextTransfer textTransfer = TextTransfer.getInstance();
-        fClipboard.setContents(new Object[] { textData },
-            new Transfer[] { textTransfer });
+		TextTransfer textTransfer = TextTransfer.getInstance();
+		fClipboard.setContents(new Object[] { textData }, new Transfer[] { textTransfer });
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.jface.dialogs.Dialog#okPressed()
 	 */
 	@Override
@@ -131,16 +202,13 @@ public class ReportIssueDialog extends TitleAreaDialog
 	@Override
 	public boolean close()
 	{
-		if (fClipboard != null) 
+		if (fClipboard != null)
 		{
 			fClipboard.dispose();
 			fClipboard = null;
 		}
-		
+
 		return super.close();
 	}
-	
-	
-	
-	
+
 }
