709dec1db9e1fbf161835c9e337465dfc140244f
Barthelemy Dagenais <bart@cs.mcgill.ca>
2010-12-14 11:24:09 -0500

    refs #103 Added a test on libreoffice rtf document import.

--- ca.mcgill.cs.swevo.qualyzer.tests/src/ca/mcgill/cs/swevo/qualyzer/handlers/ImportTranscriptHandlerTest.java
+++ ca.mcgill.cs.swevo.qualyzer.tests/src/ca/mcgill/cs/swevo/qualyzer/handlers/ImportTranscriptHandlerTest.java
@@ -44,62 +44,63 @@ import ca.mcgill.cs.swevo.qualyzer.wizards.pages.ImportTranscriptPage;
 
 /**
  * @author Jonathan Faubert
- *
+ * 
  */
 public class ImportTranscriptHandlerTest
 {
 	private Project fProject;
 	private IProject wProject;
 	private IWorkbenchPage fPage;
-	
+
 	private static final String PROJECT = "Project";
 	private static final String INV = "Inv";
 	private static final String PART = "Part";
 	private static final String DOC = "ImportDocument.rtf";
+	private static final String DOC_LIBRE_OFFICE = "ImportDocumentLibreOffice3.rtf";
 	private static final String DOC_NAME = "ImportDocument";
+	private static final String DOC_LIBRE_OFFICE_NAME = "ImportDocumentLibreOffice3";
 
 	@Before
 	public void setUp()
 	{
 		fPage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
 		fPage.closeAllEditors(false);
-		
+
 		fProject = Facade.getInstance().createProject(PROJECT, INV, "", "");
 		Facade.getInstance().createParticipant(PART, "", fProject);
-		
+
 		wProject = ResourcesPlugin.getWorkspace().getRoot().getProject(PROJECT);
 	}
-	
+
 	@After
 	public void tearDown()
 	{
 		Facade.getInstance().deleteProject(fProject);
 	}
-	
-	@Test
-	public void importTranscriptTest()
+
+	private void importTranscript(final String filePath, final String name)
 	{
 		TestUtil.setProjectExplorerSelection(wProject);
-		
+
 		ImportTranscriptHandler handler = new ImportTranscriptHandler();
 		handler.setTesting(true);
 		handler.setTester(new IDialogTester()
 		{
-			
+
 			@Override
 			public void execute(Dialog dialog)
 			{
 				QualyzerWizardDialog wizard = (QualyzerWizardDialog) dialog;
-				
+
 				ImportTranscriptPage page = (ImportTranscriptPage) wizard.getCurrentPage();
-				page.getFileText().setText(DOC);
-				page.getNameText().setText(DOC_NAME);
+				page.getFileText().setText(filePath);
+				page.getNameText().setText(name);
 				page.getTable().select(0);
-				
+
 				wizard.finishPressed();
 			}
 		});
-		
+
 		try
 		{
 			handler.execute(null);
@@ -108,18 +109,18 @@ public class ImportTranscriptHandlerTest
 		{
 			fail();
 		}
-		
+
 		fProject = PersistenceManager.getInstance().getProject(PROJECT);
 		assertEquals(fProject.getTranscripts().size(), 1);
-		
+
 		Transcript transcript = fProject.getTranscripts().get(0);
-		assertEquals(transcript.getName(), DOC_NAME);
-		assertEquals(transcript.getFileName(), DOC);
-		
+		assertEquals(transcript.getName(), name);
+		assertEquals(transcript.getFileName(), filePath);
+
 		Transcript lTranscript = Facade.getInstance().forceTranscriptLoad(transcript);
 		assertEquals(lTranscript.getParticipants().size(), 1);
 		assertEquals(lTranscript.getParticipants().get(0), fProject.getParticipants().get(0));
-		
+
 		try
 		{
 			wProject.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
@@ -128,12 +129,26 @@ public class ImportTranscriptHandlerTest
 		{
 			fail();
 		}
-		
-		IFile file = wProject.getFile("/transcripts/"+DOC);
+
+		IFile file = wProject.getFile("/transcripts/" + filePath);
 		assertTrue(file.exists());
-		
+
 		assertEquals(fPage.getEditorReferences().length, 1);
 		IEditorPart editor = fPage.getActiveEditor();
 		assertTrue(editor instanceof TranscriptEditor);
+	}
+
+	@Test
+	public void importCustomTranscriptTest()
+	{
+		// Import Custom RTF document
+		importTranscript(DOC, DOC_NAME);
+	}
+	
+	@Test
+	public void importLibreOfficeTranscriptTest()
+	{
+		// Import LibreOffice RTF document
+		importTranscript(DOC_LIBRE_OFFICE, DOC_LIBRE_OFFICE_NAME);
 	}
 }
